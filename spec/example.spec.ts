9154d1689c267ebe54a4a33a804adc0dc5a36705
Asmita <asmitad72@gmail.com>
2026-02-04 23:56:42 +0530

    Lesson17

---
+++ spec/example.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('Minimum required characters for password', async ({ page }) => {
+    await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
+    const passwordField= page.getByTestId('password-input');
+    await passwordField.pressSequentially('12345');
+    const passwordFieldError= page.locator('.form-error_active')
+
+    await expect(passwordFieldError).toBeVisible()
+})
+
+test('Incorrect credentials', async ({ page }) => {
+    await page.goto('');
+    const usernameField = page.getByTestId('username-input');
+    await usernameField.fill('invalid_user');
+    const passwordField = page.getByTestId('password-input');
+    await passwordField.fill('invalid123');
+    const signInButton = page.getByTestId('signIn-button');
+    await signInButton.click();
+
+    const errorPopup = page.getByTestId('authorizationError-popup');
+    await expect(errorPopup).toBeVisible();
+})
+
+
