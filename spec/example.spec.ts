1f2dfa4c883585dedf3a1fed23dd9911896a61ba
asmitad72 <66270213+asmitad72@users.noreply.github.com>
2026-03-07 19:27:08 +0530

    homework17-faker-js library added (#1)
    * homework17-faker-js library added
    
    * README.md file added

--- spec/example.spec.ts
+++ spec/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';//homework 17
 
 test('Minimum required characters for password', async ({ page }) => {
     await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
@@ -22,4 +23,22 @@ test('Incorrect credentials', async ({ page }) => {
     await expect(errorPopup).toBeVisible();
 })
 
+//homework17
+test('Verify error popup with random credentials', async ({ page }) => {
+    const randomUsername = faker.internet.username();
+    const randomPassword = faker.internet.password();
+
+    //await page.goto('');
+    await page.goto(process.env.APP_URL!);
+
+    const usernameField = page.getByTestId('username-input');
+    await usernameField.fill(randomUsername);
+    const passwordField = page.getByTestId('password-input');
+    await passwordField.fill(randomPassword);
+    const signInButton = page.getByTestId('signIn-button');
+    await signInButton.click();
+
+    const errorPopup = page.getByTestId('authorizationError-popup');
+    await expect(errorPopup).toBeVisible();
+});
 
